091a06f062b6be806bad22e0bb964b49d4456eb4
CRClothier <c.r.clothier@gmail.com>
2022-02-18 22:02:05 -0500

    fix linters

--- js/script.js
+++ js/script.js
@@ -1,66 +1,66 @@
-  import { Book } from './book.js'
+import { Book } from './book.js'
 
-  const addBookBtn = document.querySelector('#add');
-  addBookBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
-    if (title.value !== '' && author.value !== '') {
-      const book = new Book(title.value, author.value);
-      book.add();
-      title.value = '';
-      author.value = '';
-      Book.displayBooks();
-      localStorage.setItem('books', JSON.stringify(Book.books));
-      const successParagragh = document.querySelector('#add-book-success');
+const addBookBtn = document.querySelector('#add');
+addBookBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  if (title.value !== '' && author.value !== '') {
+    const book = new Book(title.value, author.value);
+    book.add();
+    title.value = '';
+    author.value = '';
+    Book.displayBooks();
+    localStorage.setItem('books', JSON.stringify(Book.books));
+    const successParagragh = document.querySelector('#add-book-success');
+    successParagragh.classList.toggle('d-none');
+    setTimeout(() => {
       successParagragh.classList.toggle('d-none');
-      setTimeout(() => {
-        successParagragh.classList.toggle('d-none');
-      }, 1000);
-    }
-  });
-
-  if (localStorage.getItem('books') !== null) {
-    Book.books = JSON.parse(localStorage.getItem('books'));
+    }, 1000);
   }
+});
 
-  Book.displayBooks();
-  const listLink = document.querySelector('#list-link');
-  const newLink = document.querySelector('#new-link');
-  const contactLink = document.querySelector('#contact-link');
+if (localStorage.getItem('books') !== null) {
+  Book.books = JSON.parse(localStorage.getItem('books'));
+}
 
-  const listSection = document.querySelector('.books-list');
-  const newSection = document.querySelector('.add-books');
-  const contactSection = document.querySelector('.contact');
+Book.displayBooks();
+const listLink = document.querySelector('#list-link');
+const newLink = document.querySelector('#new-link');
+const contactLink = document.querySelector('#contact-link');
 
-  listLink.addEventListener('click', () => {
-    listSection.classList.add('show');
-    newSection.classList.remove('show');
-    contactSection.classList.remove('show');
-    listLink.classList.add('active');
-    newLink.classList.remove('active');
-    contactLink.classList.remove('active)');
-  });
+const listSection = document.querySelector('.books-list');
+const newSection = document.querySelector('.add-books');
+const contactSection = document.querySelector('.contact');
 
-  newLink.addEventListener('click', () => {
-    listSection.classList.remove('show');
-    newSection.classList.add('show');
-    contactSection.classList.remove('show');
-    listLink.classList.remove('active');
-    newLink.classList.add('active');
-    contactLink.classList.remove('active)');
-  });
+listLink.addEventListener('click', () => {
+  listSection.classList.add('show');
+  newSection.classList.remove('show');
+  contactSection.classList.remove('show');
+  listLink.classList.add('active');
+  newLink.classList.remove('active');
+  contactLink.classList.remove('active)');
+});
 
-  contactLink.addEventListener('click', () => {
-    listSection.classList.remove('show');
-    newSection.classList.remove('show');
-    contactSection.classList.add('show');
-    listLink.classList.remove('active');
-    newLink.classList.remove('active');
-    contactLink.classList.add('active');
-  });
+newLink.addEventListener('click', () => {
+  listSection.classList.remove('show');
+  newSection.classList.add('show');
+  contactSection.classList.remove('show');
+  listLink.classList.remove('active');
+  newLink.classList.add('active');
+  contactLink.classList.remove('active)');
+});
 
-  const time = document.querySelector('.time');
-  const now = luxon.DateTime.now();// eslint-disable-line no-undef
-  const date = luxon.DateTime.fromISO(now);// eslint-disable-line no-undef
-  time.innerHTML = date.toLocaleString(luxon.DateTime.DATETIME_MED);// eslint-disable-line no-undef
+contactLink.addEventListener('click', () => {
+  listSection.classList.remove('show');
+  newSection.classList.remove('show');
+  contactSection.classList.add('show');
+  listLink.classList.remove('active');
+  newLink.classList.remove('active');
+  contactLink.classList.add('active');
+});
+
+const time = document.querySelector('.time');
+const now = luxon.DateTime.now();// eslint-disable-line no-undef
+const date = luxon.DateTime.fromISO(now);// eslint-disable-line no-undef
+time.innerHTML = date.toLocaleString(luxon.DateTime.DATETIME_MED);// eslint-disable-line no-undef
